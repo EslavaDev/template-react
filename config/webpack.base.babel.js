9d53f8ce852f3eaf9199bdd341497b602de011e6
daniel alejandro eslava <danieleslava52@gmail.com>
2019-05-14 18:37:08 -0500

    update webpack

--- config/webpack.base.babel.js
+++ config/webpack.base.babel.js
@@ -16,80 +16,82 @@ module.exports = (options) => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/'
+      publicPath: '/',
     },
-    options.output
+    options.output,
   ),
   module: {
     rules: [
       {
-        test: /\.tsx$/, // Transform all .tsx files required somewhere with Babel
+        test: /\.(tsx|ts)$/, // Transform all .tsx files required somewhere with Babel
         exclude: /node_modules/,
         use: {
           loader: 'awesome-typescript-loader',
           options: {
             ...options.babelQuery,
             getCustomTransformers: () => ({
-              before: [tsImportPluginFactory({
-                libraryName: 'antd',
-                libraryDirectory: 'lib',
-                style: true
-              })]
+              before: [
+                tsImportPluginFactory({
+                  libraryName: 'antd',
+                  libraryDirectory: 'lib',
+                  style: true,
+                }),
+              ],
             }),
           },
-        }
+        },
       },
       {
         // Preprocess our own .scss files
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         // Preprocess 3rd party .css files located in node_modules
         test: /\.css$/,
         include: /node_modules/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
-        use: 'file-loader'
+        use: 'file-loader',
       },
       {
         test: /\.(jpg|png|gif)$/,
-        use: 'file-loader'
+        use: 'file-loader',
       },
       {
         test: /\.html$/,
-        use: 'html-loader'
+        use: 'html-loader',
       },
       {
         test: /\.(mp4|webm)$/,
         use: {
           loader: 'url-loader',
           options: {
-            limit: 10000
-          }
-        }
-      }
-    ]
+            limit: 10000,
+          },
+        },
+      },
+    ],
   },
   plugins: options.plugins.concat([
     new webpack.ProvidePlugin({
       // make fetch available
-      fetch: 'exports-loader?self.fetch!whatwg-fetch'
+      fetch: 'exports-loader?self.fetch!whatwg-fetch',
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
-    new CheckerPlugin()
+    new CheckerPlugin(),
   ]),
   resolve: {
     modules: ['src', 'node_modules'],
     extensions: ['.ts', '.tsx', '.js', '.json', 'jsx'],
-    mainFields: ['browser', 'jsnext:main', 'main']
+    mainFields: ['browser', 'jsnext:main', 'main'],
   },
   devtool: options.devtool,
   target: 'web',
@@ -98,7 +100,7 @@ module.exports = (options) => ({
     namedModules: true,
     splitChunks: {
       name: 'vendor',
-      minChunks: 2
-    }
+      minChunks: 2,
+    },
   },
 });
